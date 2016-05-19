c4a7ce024fe9c74df4c2ba24940979f6b00640ce
Charlie Hayward and Dan Schafer <danfschafer@gmail.com>
2016-05-18 20:59:19 -0700

    add index, js and css

--- walrus/js/scripts.js
+++ walrus/js/scripts.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
     $("#initially-showing").slideToggle();
     $("#initially-hidden").slideToggle();
   });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+
+  $("button#original").click(function() {
+    $("body").removeClass();
+    $("body").addClass("body");
+  });
+
 });
